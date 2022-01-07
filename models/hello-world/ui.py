13e636bd2523af6db74ff8a961d9e02f61873387
OliverUrsell <ollie@fuzzylabs.ai>
2022-01-07 13:21:40 +0000

    Started on more complex UI

--- models/hello-world/ui.py
+++ models/hello-world/ui.py
@@ -6,6 +6,12 @@ model_url = "http://localhost:5000/predict"
 
 st.title("Sudoku Difficulty Evaluator")
 user_input = st.text_input("Sudoku string")
+
+for row in range(9):
+    columns = st.columns(9)
+    for i, column in enumerate(columns):
+        column.selectbox("", (".", 1, 2, 3, 4, 5, 6, 7, 8, 9), key=row*9 + i)
+
 if st.button("Evaluate"):
     request_data = [
         user_input
