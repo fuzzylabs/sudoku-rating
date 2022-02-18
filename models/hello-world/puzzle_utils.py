9e1eb09457e06645871f347e7ae7ad805e62538d
D. Lowl <d.lowl@d-lowl.space>
2022-02-18 13:29:50 +0300

    Pull in seldon core changes from monitoring

--- models/hello-world/puzzle_utils.py
+++ models/hello-world/puzzle_utils.py
@@ -10,3 +10,10 @@ def get_category(char: str) -> int:
 
 def prepare_puzzle(puzzle: str) -> np.ndarray:
     return np.array([get_category(char) for char in puzzle])
+
+
+def prepare_puzzle_for_monitoring(puzzle, prediction):
+    return {
+        "clues": len([digit for digit in puzzle if digit == 0]),
+        "difficulty": prediction
+    }
